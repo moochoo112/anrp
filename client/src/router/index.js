26bd6c5d45c9ef40f4b590aa69a5579fde24f2f6
Kim <565459@student.inholland.nl>
2021-06-07 14:16:20 +0200

    done

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Ping from '../components/Ping.vue';
 import Home from '../components/Home.vue';
 
 Vue.use(Router);
@@ -9,11 +8,6 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/ping',
-      name: 'Ping',
-      component: Ping,
-    },
     {
       path: '/',
       name: 'Home',
